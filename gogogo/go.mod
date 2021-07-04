673c3ae7958fd89bec94599556cc72f81ef6c1cb
Jesse Stolwijk <stolwijkjesse@gmail.com>
2021-07-04 20:26:27 +0200

    Create kv store to store shit

--- gogogo/go.mod
+++ gogogo/go.mod
@@ -1,3 +1,8 @@
 module github.com/jstolwijk/scuffed-to-do-list/gogogo
 
 go 1.15
+
+require (
+	github.com/dgraph-io/badger v1.6.2
+	github.com/dgraph-io/badger/v3 v3.2103.0 // indirect
+)
--- gogogo/main.go
+++ gogogo/main.go
@@ -5,16 +5,40 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/dgraph-io/badger"
 )
 
+func getEnvOrDefault(varName string, fallback string) string {
+	value := os.Getenv(varName)
+
+	if value == "" {
+		value = fallback
+		log.Printf("No env var %varName found, use default: %s", varName, fallback)
+	}
+
+	return value
+}
+
+func openBadgerDb() *badger.DB {
+	dbStoragePath := getEnvOrDefault("DB_STORAGE_PATH", "./database-files")
+
+	db, err := badger.Open(badger.DefaultOptions(dbStoragePath))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
 func main() {
+	db := openBadgerDb()
+	defer db.Close()
+
 	http.HandleFunc("/", indexHandler)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
+	port := getEnvOrDefault("PORT", "8080")
 
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
